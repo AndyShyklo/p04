7be68c849c6705b0a1f58b8e2bed569631578c29
Will Nzeuton <wnzeuton50@stuy.edu>
2025-03-29 00:58:40 -0400

    globe spin

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+# Will Nzeuton, Andy Shyklo, Kyle Lee, Margie Cao
+# JOY ACROSS BORDERS 🔥🔥😵‍💫 by madeinguatemala
+# SoftDev
+# p04
+# 2025-03-28
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
