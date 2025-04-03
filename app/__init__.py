5482736977a3bccb883b5d62ccc83dc552afa334
Margie Cao <mcao50@stuy.edu>
2025-04-03 11:47:43 -0400

    classwork

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@
 # SoftDev
 # p04
 # 2025-03-28
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
@@ -12,6 +12,16 @@ app.secret_key = os.urandom(32)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        print(username)
+        print(password)
+        return redirect("/login")
+    return render_template("register.html")
 
 if __name__ == "__main__":
     app.debug = True
