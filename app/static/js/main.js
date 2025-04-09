d9fd270499a1e17012b3740f87a8ef12bcf0f133
Will Nzeuton <wnzeuton50@stuy.edu>
2025-04-09 11:38:14 -0400

    fix nav

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -25,11 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
         <li class="nav-item">
           <a class="nav-link" href="/settings">⚙</a>
         </li>
-        
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+	<li class="nav-item">
+	  <a class="nav-link" href="/login">Login</a>
+	</li>        
+        <li class="nav-item">
+	  <a class="nav-link" href="/register">Register</a>
+	</li>
     </div>
   </nav>`
 
