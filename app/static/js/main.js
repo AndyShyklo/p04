95aff6eabf99ecf665bb7f7e0e2f79d218097928
Will Nzeuton <wnzeuton50@stuy.edu>
2025-04-22 20:30:41 -0400

    nav

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -8,40 +8,38 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const nav = document.getElementById("nav")
   nav.innerHTML = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">🔥🔥😵‍💫</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/">🔥🔥😵‍💫</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="/map">Map</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/profile">👤</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/settings">⚙</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/login">Login</a>
-        </li>        
-              <li class="nav-item">
-          <a class="nav-link" href="/register">Register</a>
-        </li>
-        
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-      
-      <form class="form-inline my-2 my-lg-0" method="POST" action="/logout">
-        <button class="btn btn-outline-success my-2 my-sm-0" aria-label="Logout" type="submit">Logout</button>
-      </form>
-    </div>
-  </nav>`
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/map">Map</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/profile">Reviews</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="/login">Login</a>
+      </li>        
+      <li class="nav-item">
+        <a class="nav-link" href="/register">Register</a>
+      </li>
+      <li class="nav-item">
+        <form class="form-inline my-2 my-lg-0" method="POST" action="/logout">
+          <button class="btn btn-outline-success my-2 my-sm-0" aria-label="Logout" type="submit">Logout</button>
+        </form>
+      </li>
+    </ul>
+  </div>
+</nav>
+  `
 
 });
 
